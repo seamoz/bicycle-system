62cbab7909705cdca94fc790fe43b30f4a9ee7f7
seamoz <485583043@qq.com>
2019-08-13 19:36:10 +0800

    1

--- bicycle-billing-service/src/main/java/com/ps/bicyclebillingservice/BicycleBillingServiceApplication.java
+++ bicycle-billing-service/src/main/java/com/ps/bicyclebillingservice/BicycleBillingServiceApplication.java
@@ -11,5 +11,4 @@ public class BicycleBillingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BicycleBillingServiceApplication.class, args);
     }
-
 }
