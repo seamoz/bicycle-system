a44f9d831713ece57b87d556a3d7b3b63546dac8
MeiYan-81 <2793946976@qq.com>
2019-08-15 08:30:53 +0800

    提交故障类

--- bicycle-h5-app/src/main/java/com/ps/bicycleh5app/controller/ManagebicycleController.java
+++ bicycle-h5-app/src/main/java/com/ps/bicycleh5app/controller/ManagebicycleController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author VP
+ */
 @RestController
 public class ManagebicycleController {
 
