26a8f7061d0b1db0166cb6088eb51584818a8236
zzh <1650998665@qq.com>
2019-08-16 09:17:01 +0800

    将单车模块完善到h5 1.0
    # Conflicts:
    #	bicycle-managebicycle-service/src/main/java/com/ps/bicyclemanagebicycleservice/BicycleManagebicycleServiceApplication.java
    #	bicycle-managebicycle-service/src/main/java/com/ps/bicyclemanagebicycleservice/controller/ManageBicycleController.java
    #	bicycle-managebicycle-service/src/main/java/com/ps/bicyclemanagebicycleservice/domain/Bicycle.java
    #	bicycle-managebicycle-service/src/main/java/com/ps/bicyclemanagebicycleservice/mapper/ManageBicycleMapper.java
    #	bicycle-managebicycle-service/src/main/java/com/ps/bicyclemanagebicycleservice/service/ManageBicycleService.java

--- bicycle-h5-app/src/main/java/com/ps/bicycleh5app/service/ManagebicycleService.java
+++ bicycle-h5-app/src/main/java/com/ps/bicycleh5app/service/ManagebicycleService.java
@@ -1,8 +1,12 @@
 package com.ps.bicycleh5app.service;
 
 import com.ps.allapp.domain.Result;
+import com.ps.allapp.domain.ShareBicycle;
+import com.ps.allapp.domain.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -10,6 +14,38 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @FeignClient("managebicycle-service")
 public interface ManagebicycleService {
+    /**
+     * 更改地址
+     * @param shareBicycle
+     * @return
+     */
+    @GetMapping("/bikes/address")
+    Result changeAddress(@RequestBody ShareBicycle shareBicycle);
+
+    /**
+     * 初始化首页获取地址和预约的单车
+     * @param user
+     * @return
+     */
+    @GetMapping("/bikes/init")
+    Result bicycleInit(@RequestBody User user);
+
+    /**
+     * 预约单车
+     * @param user
+     * @return
+     */
+    @PostMapping("/bikes/subscribe")
+    Result appointmentBicycle(@RequestBody User user);
+
+    /**
+     * 解锁
+     * @param user
+     * @return
+     */
+    @PostMapping("/bikes/unlock")
+    Result unlockBicycle(@RequestBody User user);
+
 
     /**
      * 历史故障（用户提交单车的故障）
