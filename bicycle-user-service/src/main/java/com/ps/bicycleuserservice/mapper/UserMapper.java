ac4838ad3d0543fe25105173bc1bfd4f93864569
MeiYan-81 <53925195+MeiYan-81@users.noreply.github.com>
2019-08-19 14:46:20 +0800

    Update UserMapper.java

--- bicycle-user-service/src/main/java/com/ps/bicycleuserservice/mapper/UserMapper.java
+++ bicycle-user-service/src/main/java/com/ps/bicycleuserservice/mapper/UserMapper.java
@@ -71,22 +71,6 @@ public interface UserMapper {
     User queryPersonage(int userId);
 
     /**
-     *  免密支付
-     * @param id
-     * @param noPasswordPay
-     * @return
-     */
-    Integer confidentialPayment(int id,int noPasswordPay);
-
-    /**
-     * 根据用户id，密码查询 钱包id
-     * @param userId
-     * @param password
-     * @return
-     */
-    Integer userWalletDetails(int userId, String password);
-
-    /**
      * @Description 根据电话修改密码
      * @param phone 用户的电话号码
      * @return User 返回的用户对象
